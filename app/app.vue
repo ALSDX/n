6d2a7736c0cdfe5c7defd9f288ac9096b9739249
ALSDX <alsdx@mail.ru>
2025-10-27 00:21:49 +0500

    why doesn't nuxtWelcome disappear

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="app">
+    <h1>Моё приложение на Nuxt</h1>
+    <p>Теперь NuxtWelcome исчез 🎉</p>
   </div>
 </template>
+
+<script setup>
+  // Твой JS/TS код, если нужно
+</script>
+
+<style>
+  .app {
+    margin-top: 120px;
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: { 'Allerta Stencil': true },
+    download: true,
+    display: 'swap',
+    preload: true,
+  },
 })
