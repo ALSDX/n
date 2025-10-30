819e5c0f6bbfb6b5aff1cdb1892468cf136d6297
ALSDX <alsdx@mail.ru>
2025-10-31 00:26:14 +0500

    chore: rename folders, add slot in default.vue, update .env.example

--- app.vue
+++ app.vue
@@ -1,16 +1,9 @@
 <template>
-  <div class="app">
-    <h1 style="font-size: 50px; font-weight: 400">Sobaka Raiz</h1>
-    <p style="font-size: 50px">Whereas disregard and contempt for human rights have resulted</p>
-  </div>
+  <NuxtLayout />
 </template>
 
 <script setup></script>
 
 <style>
-  .app {
-    margin-top: 120px;
-    font-family: 'Allerta Stencil', serif;
-    text-align: center;
-  }
+  /* style */
 </style>
